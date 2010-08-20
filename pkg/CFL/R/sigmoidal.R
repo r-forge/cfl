8a789cdc3c72339ea04ae4db08e8e51c878454bf
pablomo03 (Pablo Marin Ortega) <pablomo03@gmail.com>
2010-08-20 10:31:26 +0000

    sigmoidal function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cfl@18 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/CFL/R/sigmoidal.R
+++ pkg/CFL/R/sigmoidal.R
@@ -1,5 +1,11 @@
 sigmoidal<-
 		function(x,ganma,beta) {
+	if (any(ganma<0|ganma>1)) { 
+		stop("Error...the value is not among 0 and 1")
+	}
+	if (any(beta<0|beta>1)) { 
+		stop("Error...the value is not a among 0 and 1")
+	}
 	alfa=(log(0.9,base=exp(1))-log(0.1,base=exp(1))/(ganma-beta))
 	var<-alfa*(x-ganma)*(-1)
 	s<-1/(1+exp(var))
